5d50e6225080a8dfe8eef0530efe2acf166a0013
Brady Bridges <bradyjbridges@gmail.com>
2019-11-20 22:19:26 -0700

    Add portfolio and joiner table data to seed file

--- db/seeds/dev/coindata.js
+++ db/seeds/dev/coindata.js
@@ -11,16 +11,45 @@ const createCoinEntry = (knex, data) => {
   });
 };
 
-// coinId | date | name | price | marketCap
+const createJoiner = (knex, coinNames) => {
+    const coinPromises = [];
+    coinNames.forEach(name => {
+      coinPromises.push(knex('joiner').insert({ name }));
+    });
+    return Promise.all(coinPromises);
+};
+
+const createPortfolio = (knex, coinNames) => {
+  const coinPromises = [];
+  coinNames.forEach((name, i) => {
+    const coinId = i + 1;
+    coinPromises.push(knex('portfolio').insert({ coinId, name, qty: 0 }));
+  });
+  return Promise.all(coinPromises);
+};
+
+const createCoinData = (knex) => {
+  let coinPromises = [];
+  coinData.forEach(coin => {
+    coinPromises.push(createCoinEntry(knex, coin));
+  });
+  return Promise.all(coinPromises);
+}
 
 exports.seed = function(knex) {
   return knex('coindata').del()
+  .then(() => knex('portfolio').del())
+  .then(() => knex('joiner').del())
   .then(() => {
-    let coinPromises = [];
-    coinData.forEach(coin => {
-      coinPromises.push(createCoinEntry(knex, coin));
-    });
-    return Promise.all(coinPromises);
+    createCoinData(knex);
+  })
+  .then(() => {
+    coinNames = ['Bitcoin', 'Litecoin', 'Ethereum'];
+    return createJoiner(knex, coinNames);
+  })
+  .then(() => {
+    coinNames = ['Bitcoin', 'Litecoin', 'Ethereum'];
+    return createPortfolio(knex, coinNames);
   })
   .catch(error => console.log(`Error seeding data: ${error}`));
 };
