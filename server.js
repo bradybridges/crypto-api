d6ef8848912fd00398af9860e0709cbe8bc6e0d9
Brady Bridges <bradyjbridges@gmail.com>
2019-11-23 20:56:43 -0700

    Add cors to API to allow cross origin

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
